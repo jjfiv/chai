2f9eab8aa03ab6b1d6787df2ebb3ad3dcdc243a0
John Foley <jfoley@cs.umass.edu>
2015-01-30 23:58:31 -0500

    Move tests to java 7

--- src/test/java/edu/umass/cs/ciir/jfoley/chai/collections/ListBasedOrderedSetTest.java
+++ src/test/java/edu/umass/cs/ciir/jfoley/chai/collections/ListBasedOrderedSetTest.java
@@ -2,9 +2,10 @@ package edu.umass.cs.ciir.jfoley.chai.collections;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.TreeSet;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -17,7 +18,7 @@ public class ListBasedOrderedSetTest {
 		data.addAll(Arrays.asList(1,2,3,4,1,2,3,4,1,2,5,4,2,3));
 		assertEquals(data.size(), 5);
 		assertTrue(data.contains(1));
-		assertEquals(Arrays.<Object>asList(1, 2, 3, 4, 5), data.stream().sorted().collect(Collectors.toList()));
+		assertEquals(Arrays.asList(1, 2, 3, 4, 5), new ArrayList<>(new TreeSet<>(data)));
 	}
 
 }
