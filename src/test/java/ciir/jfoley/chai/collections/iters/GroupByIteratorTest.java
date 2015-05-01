846e0013adb5653428e34d1e875e0bd3cc8351f5
John Foley <jfoley@cs.umass.edu>
2015-05-01 12:23:25 -0400

    Clarify input data for test

--- src/test/java/ciir/jfoley/chai/collections/iters/GroupByIteratorTest.java
+++ src/test/java/ciir/jfoley/chai/collections/iters/GroupByIteratorTest.java
@@ -14,7 +14,12 @@ public class GroupByIteratorTest {
 
   @Test
   public void testLazyGroupBy() {
-    List<Integer> data = Arrays.asList(3,3,3,2,2,1,4,4,4,4,6,6,6,6,6,6);
+    List<Integer> data = Arrays.asList(
+        3,3,3,
+        2,2,
+        1,
+        4,4,4,4,
+        6,6,6,6,6,6);
 
     for (List<Integer> xs : new OneShotIterable<>(new GroupByIterator<>(data.iterator()))) {
       int value = xs.get(0);
