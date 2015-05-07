ad0413e0f6290b3ed94092becee1755df09219ff
John Foley <jfoley@cs.umass.edu>
2015-05-07 13:47:27 -0400

    Some more test coverage

--- src/test/java/ciir/jfoley/chai/collections/list/IntListTest.java
+++ src/test/java/ciir/jfoley/chai/collections/list/IntListTest.java
@@ -25,6 +25,23 @@ public class IntListTest {
 	}
 
 	@Test
+	public void testAdd2() throws Exception {
+		IntList test = new IntList();
+		test.add(1);
+		test.add(2);
+		test.add(3);
+		test.add(4);
+		test.add(5);
+
+		assertEquals(Arrays.asList(1, 2, 3, 4, 5), test);
+		int[] expected = new int[]{1, 2, 3, 4, 5};
+		assertArrayEquals(expected, test.asArray());
+		assertArrayEquals(expected, test.unsafeArray());
+		assertEquals(1, test.getQuick(0));
+		assertEquals(8, test.size);
+	}
+
+	@Test
 	public void testAddAll() {
 		List<Integer> original = Arrays.asList(1,2,3,4,5);
 		IntList test = new IntList(original);
