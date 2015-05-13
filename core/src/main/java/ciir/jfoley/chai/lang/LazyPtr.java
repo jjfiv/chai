c9a1e0eb4bbc76662f87c824158e1a0cba28cd4a
John Foley <jfoley@cs.umass.edu>
2015-05-13 19:37:53 -0400

    Create LazyPtr, format Fns

---
+++ core/src/main/java/ciir/jfoley/chai/lang/LazyPtr.java
@@ -0,0 +1,24 @@
+package ciir.jfoley.chai.lang;
+
+import ciir.jfoley.chai.fn.GenerateFn;
+
+/**
+ * A wrapper class that hides a pointer to a lazy variable. It gets initialized upon first call to get().
+ * Note: this class is *not* threadsafe.
+ * @author jfoley
+ */
+public class LazyPtr<T> implements GenerateFn<T> {
+  private final GenerateFn<T> createFn;
+  private T instance;
+  public LazyPtr(GenerateFn<T> createFn) {
+    this.createFn = createFn;
+    this.instance = null;
+  }
+  public T get() {
+    if(instance == null) {
+      instance = createFn.get();
+      if(instance == null) throw new NullPointerException("LazySingleton<T> generator gave us NULL!");
+    }
+    return instance;
+  }
+}
