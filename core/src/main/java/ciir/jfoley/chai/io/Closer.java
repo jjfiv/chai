b238973ade0ac75455faf30c1770cdc3f09fe2a7
John Foley <jfoley@cs.umass.edu>
2015-05-23 14:11:45 -0400

    Make Closer satisfy GenerateFn

--- core/src/main/java/ciir/jfoley/chai/io/Closer.java
+++ core/src/main/java/ciir/jfoley/chai/io/Closer.java
@@ -1,26 +1,29 @@
 package ciir.jfoley.chai.io;
 
+import ciir.jfoley.chai.fn.GenerateFn;
+
 /**
  * This makes an object that may or may not have a close method usable in a try-with-resources block.
  * @author jfoley.
  */
-public class Closer<T> implements AutoCloseable {
-	private final T obj;
+public class Closer<T> implements AutoCloseable, GenerateFn<T> {
+  private final T obj;
 
-	public Closer(T obj) {
-		this.obj = obj;
-	}
+  public Closer(T obj) {
+    this.obj = obj;
+  }
 
-	public T get() {
-		return obj;
-	}
+  @Override
+  public T get() {
+    return obj;
+  }
 
-	@Override
-	public void close() {
-		IO.close(obj);
-	}
+  @Override
+  public void close() {
+    IO.close(obj);
+  }
 
-	public static <T> Closer<T> of(T input) {
-		return new Closer<>(input);
-	}
+  public static <T> Closer<T> of(T input) {
+    return new Closer<>(input);
+  }
 }
--- core/src/main/java/ciir/jfoley/chai/io/IO.java
+++ core/src/main/java/ciir/jfoley/chai/io/IO.java
@@ -12,83 +12,88 @@ import java.io.*;
  */
 public class IO extends Module {
 
-	/** Used for transfer operations */
-	public static int BUFFER_SIZE = 8192;
+  /** Used for transfer operations */
+  public static int BUFFER_SIZE = 8192;
 
-	/** This class exists only to provide static methods. */
-	private IO() {}
+  /** This class exists only to provide static methods. */
+  private IO() {}
 
-	/**
-	 * Close anything!
-	 * TODO: close with reflection if available?
-	 */
-	public static void close(Object obj) {
-		try {
-			if (obj != null) {
-				if (obj instanceof Closeable) {
-					((Closeable) obj).close();
-				} else if (obj instanceof AutoCloseable) {
-					((AutoCloseable) obj).close();
-				} else if(obj instanceof XMLStreamWriter) {
-					((XMLStreamWriter) obj).close();
-				}
-			}
-		} catch (Exception ex) {
-			throw new FatalError(ex);
-		}
-	}
+  /**
+   * Close anything!
+   * TODO: close with reflection if available?
+   */
+  public static void close(Object obj) {
+    try {
+      if(obj == null) return;
+      if (obj instanceof Closeable) {
+        ((Closeable) obj).close();
+      } else if (obj instanceof AutoCloseable) {
+        ((AutoCloseable) obj).close();
+      } else if(obj instanceof XMLStreamWriter) {
+        ((XMLStreamWriter) obj).close();
+      }
+    } catch (Exception ex) {
+      throw new FatalError(ex);
+    }
+  }
 
-	/** Read all data or "slurp" a BufferedReader. */
-	public static String readAll(Reader reader, int bufferSize) throws IOException {
-		final StringBuilder contents = new StringBuilder();
-		char buf[] = new char[bufferSize];
-		while(true) {
-			int amt = reader.read(buf);
-			if(amt < 0) break;
-			contents.append(buf, 0, amt);
-			if(amt < buf.length) break;
-		}
-		return contents.toString();
-	}
+  /**
+   * Read all data or "slurp" a Reader.
+   * @param reader the Reader to read from; doesn't have to be buffered.
+   * @param bufferSize the amount of bytes to read at once, usually 4k or so.
+   * @return the total read character data as a String.
+   * @throws IOException
+   */
+  public static String readAll(Reader reader, int bufferSize) throws IOException {
+    final StringBuilder contents = new StringBuilder();
+    char buf[] = new char[bufferSize];
+    while(true) {
+      int amt = reader.read(buf);
+      if(amt < 0) break;
+      contents.append(buf, 0, amt);
+      if(amt < buf.length) break;
+    }
+    return contents.toString();
+  }
 
-	public static String readAll(Reader reader) throws IOException {
-		return readAll(reader, BUFFER_SIZE);
-	}
+  public static String readAll(Reader reader) throws IOException {
+    return readAll(reader, BUFFER_SIZE);
+  }
 
-	/** Slurp a file, inspired by Clojure's naming. */
-	public static String slurp(File path) throws IOException {
-		try(Reader reader = openReader(path.getAbsolutePath())) {
-			return readAll(reader);
-		}
-	}
+  /** Slurp a file, inspired by Clojure's naming. */
+  public static String slurp(File path) throws IOException {
+    try(Reader reader = openReader(path.getAbsolutePath())) {
+      return readAll(reader);
+    }
+  }
 
-	/** Spit a string to a file, inspired by Clojure's naming. */
-	public static void spit(String data, File output) throws IOException {
-		try (PrintWriter pw = openPrintWriter(output.getAbsolutePath())) {
-			pw.print(data);
-		}
-	}
+  /** Spit a string to a file, inspired by Clojure's naming. */
+  public static void spit(String data, File output) throws IOException {
+    try (PrintWriter pw = openPrintWriter(output.getAbsolutePath())) {
+      pw.print(data);
+    }
+  }
 
-	public static BufferedReader openReader(String file) throws IOException {
-		return new BufferedReader(new InputStreamReader(openInputStream(file), Encodings.UTF8));
-	}
+  public static BufferedReader openReader(String file) throws IOException {
+    return new BufferedReader(new InputStreamReader(openInputStream(file), Encodings.UTF8));
+  }
 
-	public static InputStream openInputStream(String file) throws IOException {
-		return CompressionCodec.openInputStream(file);
-	}
-	public static OutputStream openOutputStream(String file) throws IOException {
-		return CompressionCodec.openOutputStream(file);
-	}
+  public static InputStream openInputStream(String file) throws IOException {
+    return CompressionCodec.openInputStream(file);
+  }
+  public static OutputStream openOutputStream(String file) throws IOException {
+    return CompressionCodec.openOutputStream(file);
+  }
 
-	public static PrintWriter openPrintWriter(String file) throws IOException {
-		return new PrintWriter(openOutputStream(file));
-	}
+  public static PrintWriter openPrintWriter(String file) throws IOException {
+    return new PrintWriter(openOutputStream(file));
+  }
 
-	public static BufferedReader openReader(File fp) throws IOException {
-		return openReader(fp.getAbsolutePath());
-	}
+  public static BufferedReader openReader(File fp) throws IOException {
+    return openReader(fp.getAbsolutePath());
+  }
 
-	public static BufferedReader stringReader(String s) {
-		return new BufferedReader(new StringReader(s));
-	}
+  public static BufferedReader stringReader(String s) {
+    return new BufferedReader(new StringReader(s));
+  }
 }
