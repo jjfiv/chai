143c26fbb75710a71c05de60146a8d01eb170657
John Foley <jfoley@cs.umass.edu>
2015-06-12 17:07:00 -0400

    Add finalize() calls that make sure you cleaned up your resources.

--- core/src/main/java/ciir/jfoley/chai/io/TemporaryFile.java
+++ core/src/main/java/ciir/jfoley/chai/io/TemporaryFile.java
@@ -3,13 +3,17 @@ package ciir.jfoley.chai.io;
 import java.io.Closeable;
 import java.io.File;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Create and delete temporary files as resources.
  * @author jfoley.
  */
 public class TemporaryFile implements Closeable {
+	private Logger logger = Logger.getLogger(TemporaryFile.class.getName());
 	private final File file;
+	private boolean closed = false;
 
 	/** When you only care about the suffix that gets generated. */
 	public TemporaryFile(String suffix) throws IOException {
@@ -29,7 +33,15 @@ public class TemporaryFile implements Closeable {
 	}
 
 	@Override
+	public void finalize() throws Throwable {
+		super.finalize();
+		if(!closed) logger.log(Level.SEVERE, "Leaked TemporaryFile!");
+		assert(closed);
+	}
+
+	@Override
 	public void close() throws IOException {
+		closed = true;
 		boolean status = file.delete();
 		if(!status) throw new IOException("Temporary file failed to delete.");
 	}
