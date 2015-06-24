fbeb9c38cb4d500e6f44f0f3bc00d345ac3e0c1b
John Foley <jfoley@cs.umass.edu>
2015-06-24 09:41:25 -0400

    Add a comment

---
+++ core/src/main/java/ciir/jfoley/chai/lang/annotations/UsedByReflection.java
@@ -0,0 +1,8 @@
+package ciir.jfoley.chai.lang.annotations;
+
+/**
+ * Marks constructors or methods that appear unused in an IDE or other warnings but are known to be used by reflection.
+ * @author jfoley.
+ */
+public @interface UsedByReflection {
+}
