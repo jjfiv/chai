1aeb12edec457684919ae1c9b10dfb2c7f177e4a
John Foley <jfoley@cs.umass.edu>
2016-01-10 14:22:43 -0500

    AChaiList::chooseRandomly

--- core/src/main/java/ciir/jfoley/chai/collections/list/AChaiList.java
+++ core/src/main/java/ciir/jfoley/chai/collections/list/AChaiList.java
@@ -3,6 +3,7 @@ package ciir.jfoley.chai.collections.list;
 import ciir.jfoley.chai.collections.chained.ChaiIterable;
 
 import java.util.AbstractList;
+import java.util.Random;
 
 /**
  * @author jfoley.
@@ -10,5 +11,10 @@ import java.util.AbstractList;
 public abstract class AChaiList<T> extends AbstractList<T> {
 	public ChaiIterable<T> chai() {
 		return ChaiIterable.create(this);
+	}
+
+
+	public T chooseRandomly(Random rand) {
+		return this.get(rand.nextInt(this.size()));
 	}
 }
