9844a305d54193eeb810badedc82fb052e0b5f1a
John Foley <jfoley@cs.umass.edu>
2016-09-01 16:54:17 -0400

    static xml builder factory

--- xml/src/main/java/ciir/jfoley/chai/xml/ChaiXML.java
+++ xml/src/main/java/ciir/jfoley/chai/xml/ChaiXML.java
@@ -1,6 +1,5 @@
 package ciir.jfoley.chai.xml;
 
-import ciir.jfoley.chai.fn.GenerateFn;
 import ciir.jfoley.chai.lang.LazyPtr;
 import ciir.jfoley.chai.lang.Module;
 import org.xml.sax.SAXException;
@@ -14,12 +13,7 @@ import java.nio.charset.StandardCharsets;
  * @author jfoley.
  */
 public class ChaiXML extends Module {
-	static LazyPtr<DocumentBuilderFactory> factory = new LazyPtr<>(new GenerateFn<DocumentBuilderFactory>() {
-		@Override
-		public DocumentBuilderFactory get() {
-			return DocumentBuilderFactory.newInstance();
-		}
-	});
+	static LazyPtr<DocumentBuilderFactory> factory = new LazyPtr<>(DocumentBuilderFactory::newInstance);
 
   public static XNode fromFile(File fp) throws ParserConfigurationException, IOException, SAXException {
 		return fromStream(new FileInputStream(fp));
