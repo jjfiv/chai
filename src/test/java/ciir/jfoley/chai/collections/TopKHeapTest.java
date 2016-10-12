e365ae878ccbf4533fe06f3bb6e27b2ff7fefc3a
John Foley <jfoley@cs.umass.edu>
2016-10-12 09:41:46 -0400

    More coverage for TopKHeap

--- src/test/java/ciir/jfoley/chai/collections/TopKHeapTest.java
+++ src/test/java/ciir/jfoley/chai/collections/TopKHeapTest.java
@@ -1,11 +1,16 @@
 package ciir.jfoley.chai.collections;
 
 import ciir.jfoley.chai.collections.util.Comparing;
+import ciir.jfoley.chai.collections.util.ListFns;
+import gnu.trove.map.hash.TObjectIntHashMap;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class TopKHeapTest {
 
@@ -14,7 +19,9 @@ public class TopKHeapTest {
     TopKHeap<Integer> heap = new TopKHeap<>(3, Comparing.<Integer>defaultComparator());
     for (int i = 0; i < 20; i++) {
       if(i > 0) {
-        assertEquals(Math.max(0, i - 3), heap.peek().intValue());
+        Integer actual = heap.peek();
+        assertNotNull(actual);
+        assertEquals(Math.max(0, i - 3), actual.intValue());
       }
       heap.offer(i);
     }
@@ -28,5 +35,29 @@ public class TopKHeapTest {
 
     heap.addAll(IntRange.exclusive(0, 20).chai().reverse().intoList());
     assertEquals(Arrays.asList(19, 18, 17), heap.getSorted());
+  }
+
+
+  @Test
+  public void testTopCharacters() throws Exception {
+    TObjectIntHashMap<Character> items = new TObjectIntHashMap<>();
+    int z = 'z';
+    int a = 'a';
+    for (int i = a; i <= z; i++) {
+      items.put((char) i, i);
+    }
+
+    List<TopKHeap.Weighted<Character>> weighteds = TopKHeap.takeTop(10, items);
+
+    List<Character> truth = new ArrayList<>();
+    for (char c : "zyxwvutsrq".toCharArray()) { truth.add(c); }
+    assertEquals(truth, ListFns.map(weighteds, x -> x.object));
+
+    // by construction:
+    for (TopKHeap.Weighted<Character> weighted : weighteds) {
+      int c = (int) weighted.weight;
+      assertEquals(c, weighted.object.charValue());
+    }
+
   }
 }
